ecf03cfd8026fd5b1ed7b14545d270d32c469a31
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-03-21 14:46:36 -0500

    👷

--- src/programa.ts
+++ src/programa.ts
@@ -52,13 +52,34 @@ async function inicio() {
   await crearTextura('flores');
   await crearTextura('arbolLloron');
   await crearTextura('hueco1');
+  await crearTextura('manoPajaro');
+  await crearTextura('nube1');
+  await crearTextura('arbolNube');
+  await crearTextura('arbolNubeLoop');
+  await crearTextura('pajaroBailarin');
+  await crearTextura('cocoCantaor');
+  await crearTextura('mar');
+  await crearTextura('dimensionAstral');
+  await crearTextura('fondo5');
+  await crearTextura('arbolSeco');
+  await crearTextura('arbolSeco2');
+  await crearTextura('arbolSeco3');
+  await crearTextura('moscas');
+  await crearTextura('pajarera');
+  await crearTextura('floresitas');
+  await crearTextura('matorral');
+  await crearTextura('hueco2');
+  await crearTextura('huecoSol');
+  await crearTextura('arbolLloronLoop');
+  await crearTextura('huecoNube');
+  await crearTextura('hueco3');
+  await crearTextura('mesaPajaro');
+  await crearTextura('florBailarina');
 
   const fondo1 = crearSecuencia('fondo1');
   fondo1.animationSpeed = 0.1666;
-  // fondo1.anchor.set(0.5);
   fondo1?.scale.set(0.3);
   fondo1.y = dims.pasoY * 2;
-  // fondo1.scale.set(window.innerHeight / fondo1.texture.orig.height);
   fondo1.play();
   fondo1.blendMode = BLEND_MODES.MULTIPLY;
   aplicacion.stage.addChild(fondo1);
@@ -84,7 +105,7 @@ async function inicio() {
   const escupePajaro = crearSecuencia('montañaPajaro');
   escupePajaro.animationSpeed = 0.1666;
   escupePajaro?.scale.set(0.2);
-  escupePajaro.position.set(dims.pasoX * 4, dims.pasoY * 6.8);
+  escupePajaro.position.set(dims.pasoX * 4, dims.pasoY * 6.4);
   escupePajaro?.play();
   escupePajaro.alpha = 0.7;
   escupePajaro.blendMode = BLEND_MODES.MULTIPLY;
@@ -101,7 +122,7 @@ async function inicio() {
   const arbolSol = crearSecuencia('arbolSol');
   arbolSol.animationSpeed = 0.1666;
   arbolSol?.scale.set(0.1);
-  arbolSol.position.set(dims.pasoX * 1, dims.pasoY * 2);
+  arbolSol.position.set(dims.pasoX * 0.9, dims.pasoY * 2.4);
   arbolSol?.play();
   arbolSol.blendMode = BLEND_MODES.MULTIPLY;
   aplicacion.stage.addChild(arbolSol);
@@ -127,7 +148,7 @@ async function inicio() {
   const montañaArboles = crearSecuencia('montañaArboles');
   montañaArboles.animationSpeed = 0.1;
   montañaArboles?.scale.set(0.25);
-  montañaArboles.position.set(dims.pasoX * 2, dims.pasoY * 8);
+  montañaArboles.position.set(dims.pasoX * 3, dims.pasoY * 8);
   montañaArboles?.play();
   montañaArboles.blendMode = BLEND_MODES.MULTIPLY;
   aplicacion.stage.addChild(montañaArboles);
@@ -212,6 +233,190 @@ async function inicio() {
   hueco1.blendMode = BLEND_MODES.MULTIPLY;
   aplicacion.stage.addChild(hueco1);
 
+  const manoPajaro = crearSecuencia('manoPajaro');
+  manoPajaro.animationSpeed = 0.1666;
+  manoPajaro?.scale.set(0.25);
+  manoPajaro.position.set(dims.pasoX * 6, dims.pasoY * 8);
+  manoPajaro?.play();
+  manoPajaro.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(manoPajaro);
+
+  const nube1 = crearSecuencia('nube1');
+  nube1.animationSpeed = 0.1666;
+  nube1?.scale.set(0.25);
+  nube1.position.set(dims.pasoX * 5, dims.pasoY * 0.2);
+  nube1?.play();
+  nube1.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(nube1);
+
+  const arbolNube = crearSecuencia('arbolNube');
+  arbolNube.animationSpeed = 0.1666;
+  arbolNube?.scale.set(0.32);
+  arbolNube.position.set(dims.pasoX * 0.2, dims.pasoY * 4);
+  arbolNube?.play();
+  arbolNube.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(arbolNube);
+
+  const arbolNubeLoop = crearSecuencia('arbolNubeLoop');
+  arbolNubeLoop.animationSpeed = 0.116;
+  arbolNubeLoop?.scale.set(0.33);
+  arbolNubeLoop.position.set(dims.pasoX * 10, dims.pasoY * 4.7);
+  arbolNubeLoop?.play();
+  arbolNubeLoop.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(arbolNubeLoop);
+
+  const pajaroBailarin = crearSecuencia('pajaroBailarin');
+  pajaroBailarin.animationSpeed = 0.12;
+  pajaroBailarin?.scale.set(0.5);
+  pajaroBailarin.position.set(dims.pasoX * 7, dims.pasoY * 4);
+  pajaroBailarin?.play();
+  pajaroBailarin.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(pajaroBailarin);
+
+  const cocoCantaor = crearSecuencia('cocoCantaor');
+  cocoCantaor.animationSpeed = 0.166;
+  cocoCantaor?.scale.set(0.43);
+  cocoCantaor.position.set(dims.pasoX * 3, dims.pasoY * 9);
+  cocoCantaor?.play();
+  cocoCantaor.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(cocoCantaor);
+
+  const mar = crearSecuencia('mar');
+  mar.animationSpeed = 0.166;
+  mar?.scale.set(0.43);
+  mar.position.set(dims.pasoX * 7.8, dims.pasoY * 5.5);
+  mar?.play();
+  mar.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(mar);
+
+  const dimensionAstral = crearSecuencia('dimensionAstral');
+  dimensionAstral.animationSpeed = 0.166;
+  dimensionAstral?.scale.set(0.43);
+  dimensionAstral.position.set(dims.pasoX * 7.6, dims.pasoY * 8);
+  dimensionAstral?.play();
+  dimensionAstral.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(dimensionAstral);
+
+  const fondo5 = crearSecuencia('fondo5');
+  fondo5.animationSpeed = 0.166;
+  fondo5?.scale.set(0.43);
+  fondo5.position.set(dims.pasoX * 8.4, dims.pasoY * 1);
+  fondo5?.play();
+  fondo5.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(fondo5);
+
+  const arbolSeco = crearSecuencia('arbolSeco');
+  arbolSeco.animationSpeed = 0.166;
+  arbolSeco?.scale.set(0.25);
+  arbolSeco.position.set(dims.pasoX * 9.5, dims.pasoY * 4.7);
+  arbolSeco?.play();
+  arbolSeco.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(arbolSeco);
+
+  const arbolSeco2 = crearSecuencia('arbolSeco2');
+  arbolSeco2.animationSpeed = 0.166;
+  arbolSeco2?.scale.set(0.43);
+  arbolSeco2.position.set(dims.pasoX * 8.3, dims.pasoY * 4.1);
+  arbolSeco2?.play();
+  arbolSeco2.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(arbolSeco2);
+
+  const arbolSeco3 = crearSecuencia('arbolSeco3');
+  arbolSeco3.animationSpeed = 0.166;
+  arbolSeco3?.scale.set(0.23);
+  arbolSeco3.position.set(dims.pasoX * 4.3, dims.pasoY * 2.5);
+  arbolSeco3?.play();
+  arbolSeco3.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(arbolSeco3);
+
+  const moscas = crearSecuencia('moscas');
+  moscas.animationSpeed = 0.116;
+  moscas?.scale.set(0.53);
+  moscas.position.set(dims.pasoX * 6, dims.pasoY * 6);
+  moscas?.play();
+  moscas.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(moscas);
+
+  const pajarera = crearSecuencia('pajarera');
+  pajarera.animationSpeed = 0.116;
+  pajarera?.scale.set(0.43);
+  pajarera.position.set(dims.pasoX * 8, dims.pasoY * 4);
+  pajarera?.play();
+  pajarera.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(pajarera);
+
+  const floresitas = crearSecuencia('floresitas');
+  floresitas.animationSpeed = 0.116;
+  floresitas?.scale.set(0.33);
+  floresitas.position.set(dims.pasoX * 0.2, dims.pasoY * 9);
+  floresitas?.play();
+  floresitas.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(floresitas);
+
+  const matorral = crearSecuencia('matorral');
+  matorral.animationSpeed = 0.116;
+  matorral?.scale.set(0.33);
+  matorral.position.set(dims.pasoX * 9.7, dims.pasoY * 7.2);
+  matorral?.play();
+  matorral.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(matorral);
+
+  const hueco2 = crearSecuencia('hueco2');
+  hueco2.animationSpeed = 0.116;
+  hueco2?.scale.set(0.23);
+  hueco2.position.set(dims.pasoX * 4.6, dims.pasoY * 9.8);
+  hueco2?.play();
+  hueco2.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(hueco2);
+
+  const huecoSol = crearSecuencia('huecoSol');
+  huecoSol.animationSpeed = 0.166;
+  huecoSol?.scale.set(0.33);
+  huecoSol.position.set(dims.pasoX * 0.6, dims.pasoY * 5.8);
+  huecoSol?.play();
+  huecoSol.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(huecoSol);
+
+  const arbolLloronLoop = crearSecuencia('arbolLloronLoop');
+  arbolLloronLoop.animationSpeed = 0.166;
+  arbolLloronLoop?.scale.set(0.13);
+  arbolLloronLoop.position.set(dims.pasoX * 9.1, dims.pasoY * 0.5);
+  arbolLloronLoop?.play();
+  arbolLloronLoop.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(arbolLloronLoop);
+
+  const huecoNube = crearSecuencia('huecoNube');
+  huecoNube.animationSpeed = 0.166;
+  huecoNube?.scale.set(0.13);
+  huecoNube.position.set(dims.pasoX * 5.6, dims.pasoY * 2.5);
+  huecoNube?.play();
+  huecoNube.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(huecoNube);
+
+  const hueco3 = crearSecuencia('hueco3');
+  hueco3.animationSpeed = 0.166;
+  hueco3?.scale.set(0.05);
+  hueco3.position.set(dims.pasoX * 0.1, dims.pasoY * 3.5);
+  hueco3?.play();
+  hueco3.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(hueco3);
+
+  const mesaPajaro = crearSecuencia('mesaPajaro');
+  mesaPajaro.animationSpeed = 0.116;
+  mesaPajaro?.scale.set(0.35);
+  mesaPajaro.position.set(dims.pasoX * 6.4, dims.pasoY * 3.1);
+  mesaPajaro?.play();
+  mesaPajaro.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(mesaPajaro);
+
+  const florBailarina = crearSecuencia('florBailarina');
+  florBailarina.animationSpeed = 0.1;
+  florBailarina?.scale.set(0.35);
+  florBailarina.position.set(dims.pasoX * 7.2, dims.pasoY * 8.3);
+  florBailarina?.play();
+  florBailarina.blendMode = BLEND_MODES.MULTIPLY;
+  aplicacion.stage.addChild(florBailarina);
+
   // const fondo1 = crearSecuencia('fondo1');
   // // const escupePajaro = crearSecuencia('montañaPajaro');
   // if (!fondo1) return;
@@ -222,11 +427,11 @@ async function inicio() {
   // // fondo1.blendMode = BLEND_MODES.ADD;
   // aplicacion.stage.addChild(fondo1);
 
-  // for (let i = 0; i < 20; i++) {
+  // for (let i = 0; i < 1500; i++) {
   //   const clonsito = crearSecuencia('montañaPajaro');
   //   clonsito.animationSpeed = 0.2;
-  //   clonsito.x = Math.random() * aplicacion.screen.width - 500;
-  //   clonsito.y = Math.random() * aplicacion.screen.height - 500;
+  //   clonsito.x = Math.random() * aplicacion.screen.width;
+  //   clonsito.y = Math.random() * aplicacion.screen.height;
   //   // clonsito.scale.set(Math.random());
   //   clonsito.blendMode = BLEND_MODES.MULTIPLY;
 
